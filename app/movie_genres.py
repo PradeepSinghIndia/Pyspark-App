60354b2dd994caa90618f263b36098299f2fcf4f
pradeep <jnu.cdac@gmail.com>
2021-04-13 00:13:46 +0530

    datapath change

--- app/movie_genres.py
+++ app/movie_genres.py
@@ -11,14 +11,14 @@ spark = SparkSession.builder.appName(config.get("app_name")).getOrCreate()
 raw_df = (
     spark.read.format("csv")
     .option("header", "true")
-    .load(f"./{config.get('source_data_path')}/movies.csv")
+    .load(f"../{config.get('source_data_path')}/movies.csv")
 )
 
 transformed_df = raw_df.select(
     col("movieId"), explode(split(col("genres"), "\\|")).alias("genre")
 )
 
-transformed_df.write.mode("overwrite").parquet(f"./{config.get('source_data_path')}/movies_genres")
+transformed_df.write.mode("overwrite").parquet(f"../{config.get('source_data_path')}/movies_genres")
 
 
 
