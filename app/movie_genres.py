7341274cb3bc6a9309651502598521e00e8debfb
pradeep <jnu.cdac@gmail.com>
2021-04-12 23:39:32 +0530

    version-2.0 with config file

--- app/movie_genres.py
+++ app/movie_genres.py
@@ -2,20 +2,23 @@
 # coding: utf-8
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, split, explode
+import json
 
-spark = SparkSession.builder.appName("Movies").getOrCreate()
+with open("config.json","r") as config_file:
+    config=json.load(config_file)# load as a python dictionary
+spark = SparkSession.builder.appName(config.get("app_name")).getOrCreate()
 
 raw_df = (
     spark.read.format("csv")
     .option("header", "true")
-    .load("./data/ml-latest-small/movies.csv")
+    .load(f"./{config.get('source_data_path')}/movies.csv")
 )
 
 transformed_df = raw_df.select(
     col("movieId"), explode(split(col("genres"), "\\|")).alias("genre")
 )
 
-transformed_df.write.mode("overwrite").parquet("./data/output/movies_genres")
+transformed_df.write.mode("overwrite").parquet(f"./{config.get('source_data_path')}/movies_genres")
 
 
 
--- app/movies.py
+++ app/movies.py
@@ -2,23 +2,23 @@
 # coding: utf-8
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, expr
-# create spark context or get allready created
-spark = SparkSession.builder.appName("Movies").getOrCreate()
-#read csv file and create a dataframe
+import json
+with open("config.json","r") as config_file:
+    config=json.load(config_file)# load as a python dictionary
+spark = SparkSession.builder.appName(config.get("app_name")).getOrCreate()  
 raw_df = (
     spark.read.format("csv")
     .option("header", "true")
-    .load("../data/ml-latest-small/movies.csv")
+    .load(f"../{config.get('source_data_path')}/movies.csv")
 )
-# select movies columns and manipulate it
+
 transformed_df = raw_df.select(
     col("movieId"),
     expr("substring(title, 1, length(title)-6)").alias("title"),
     col("title").substr(-5, 4).alias("year"),
 )
-# write data in parquet format
-transformed_df.write.mode("overwrite").parquet("../data/output/movies")
 
+transformed_df.write.mode("overwrite").parquet(f"../{config.get('source_data_path')}/movies")
 
 
 
